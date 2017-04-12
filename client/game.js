6b00f9f3bfbee5530dd9e4994e7b930fea9154fe
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-04-11 23:22:32 -0400

    add socket.io client side and stablish a connection

--- client/game.js
+++ client/game.js
@@ -1,7 +1,13 @@
-setup = () => {
+var socket;
 
+setup = () => {
+  socket = io.connect('http://localhost:3000/');
+  createCanvas(600, 600);
+  background(51);
 }
 
 draw = () => {
-
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 20);
 }
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var app = express();
-
 var socket = require('socket.io');
+
+var app = express();
 
 // create server
 var server = app.listen(3000);
-
+console.log('Server is now running...');
 // serve static files
 app.use(express.static('client'));
+
+var io = socket(server);
+
+io.sockets.on('connection', (data) => {
+  console.log('New connection: ', data.id);
+});
